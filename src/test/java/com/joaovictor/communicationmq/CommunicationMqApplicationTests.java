46c631b48b00165899d4bfbd797b32ddcb6df9a5
João Victor Bezerra da Silva <dev.joaovictor@hotmail.com>
2024-08-27 15:32:27 +0100

    initial commit

---
+++ src/test/java/com/joaovictor/communicationmq/CommunicationMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaovictor.communicationmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunicationMqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
